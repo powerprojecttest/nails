6bd1a259207247ef9b317d5614d9775934e7049b
Yana1006 <yana.halimonenko@gmail.com>
2019-10-10 10:12:00 +0300

    myformstyle

--- index.php
+++ index.php
@@ -74,6 +74,7 @@ $mail->SMTPOptions = array(
 	  <link href="//allfont.ru/allfont.css?fonts=corinthia" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="forma.css">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"
     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
     crossorigin="anonymous"></script>
