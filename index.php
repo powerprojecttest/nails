7e138fc1c22abff9fb5ecfb2e5d04aa7a0d81369
Yana1006 <yana.halimonenko@gmail.com>
2019-10-11 11:33:18 +0300

    add price flag

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ if(isset($_POST['buttonform'])){
 	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 	  <link href="//allfont.ru/allfont.css?fonts=corinthia" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="forma.css">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"
     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
